ad8e032a7af7764e22df9b7c720c18a9e684fe73
Sharon Loh <sharon_loh@vecapital.asia>
2022-10-29 14:54:16 +0800

    Created grids

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,18 @@
 <x-app-layout>
 
     <div class="flex justify-center items-center h-full w-full mt-10">
-        <div class="grid grid-cols-4 gap-4">
-            <div>
-                hi
-            </div>
-
-            <div>
-                hi2
-            </div>
-
-            <div>
-                hi3
-            </div>
-        </div>
+        <div class="grid-column">
+            @for ($i = 0; $i < 9; $i++)
+                <div class="grid-item">
+                    <div class="product-image">
+                        hello
+                    </div>
+                    <div class="bottom-container">
+                        <div class="like-button">hi</div>
+                        <div class="share-button">hi</div>
+                    </div>
+                </div>
+            @endfor
+          </div>
     </div>
 </x-app-layout>
